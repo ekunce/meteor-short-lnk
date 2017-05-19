de8cfaa8b64ef6f74ab06f088fa9189431d3b6d7
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-19 16:21:37 +0200

    initial commit

---
+++ imports/ui/Link.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Accounts } from 'meteor/accounts-base';
+
+class Link extends React.Component {
+  onLogout () {
+      Accounts.logout();
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Your links</h1>
+        <button onClick={this.onLogout.bind(this)}>Logout</button>
+      </div>
+    );
+  }
+}
+
+export default Link;
