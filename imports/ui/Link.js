f16058d7ffc3a99001f3979a0d7592f1db5b78c0
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-22 13:49:16 +0200

    adding server side methods to validate and insert data

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -15,8 +15,7 @@ class Link extends React.Component {
     e.preventDefault();
 
     if (url) {
-      Links.insert({ url, userId: Meteor.userId() });
-
+      Meteor.call('links.insert', url);
       this.refs.url.value = '';
     }
   }
