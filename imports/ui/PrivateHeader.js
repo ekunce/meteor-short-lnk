255f1147bb9be34fd0f13d94020feb221ea6e057
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-24 14:56:03 +0200

    Link refactoring

---
+++ imports/ui/PrivateHeader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Accounts } from 'meteor/accounts-base';
+
+
+class PrivateHeader extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onLogout() {
+    Accounts.logout();
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.props.title}</h1>
+        <button onClick={this.onLogout.bind(this)}>Logout</button>
+      </div>
+    );
+  }
+}
+
+PrivateHeader.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default PrivateHeader;
