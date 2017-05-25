944f5c5f39eae3f4d8f66926e0c89e1bd0c2e4b2
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-25 13:40:35 +0200

    hiding list - basic functionality

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
@@ -28,8 +29,14 @@ class LinksListItem extends React.Component {
         <div>
           <p>{this.props.url}</p>
           <p>{this.props.shortUrl}</p>
+          <p>{this.props.visible.toString()}</p>
           <button ref="copy" data-clipboard-text={this.props.shortUrl}>
             {this.state.justCopied ? "Copied" : "Copy"}
+          </button>
+          <button onClick={() => {
+            Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
+          }}>
+            {this.props.visible ? 'Hide' : 'Unhide'}
           </button>
         </div>
       );
@@ -40,6 +47,7 @@ LinksListItem.propTypes = {
   _id: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   userId: PropTypes.string.isRequired,
+  visible: PropTypes.bool.isRequired,
   shortUrl: PropTypes.string.isRequired
 }
 
