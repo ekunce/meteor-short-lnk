b53bfeb568989f6417c3ba2d855b1a0a6ae9f773
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-25 11:42:56 +0200

    Refactoring of LinksListItem

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Links } from '../api/links';
+import LinksListItem from './LinksListItem';
 
 class LinksList extends React.Component {
   constructor(props) {
@@ -15,7 +16,7 @@ class LinksList extends React.Component {
 
   componentDidMount() {
     this.linksTracker = Tracker.autorun(() => {
-      Meteor.subscribe('links');      
+      Meteor.subscribe('links');
       const links = Links.find().fetch();
       this.setState({ links });
     })
@@ -27,7 +28,9 @@ class LinksList extends React.Component {
 
   renderLinksListItems() {
     return this.state.links.map((link) => {
-      return <p key={link._id}>{link.url}</p>
+      // return <p key={link._id}>{link.url}</p>
+      const shortUrl = Meteor.absoluteUrl(link._id);
+      return <LinksListItem key={link._id} shortUrl={shortUrl}{...link}/>;
     });
   }
 
